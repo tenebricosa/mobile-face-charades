e937d62b92b83b11e74b15f374a863628f029453
tenebricosa <elliot.wrt@gmail.com>
2019-07-19 15:15:51 +0500

    Доделала игру

---
+++ config.js
@@ -0,0 +1,539 @@
+﻿const config = {
+    level: "general",
+    time: 20000,
+    lives: 3
+};
+
+const levels = {
+    "general": [{
+        "name": "Nokia 3310",
+        "answers": [
+            "img/content/3310.png",
+            "img/content/N70.png",
+            "img/content/3250.webp",
+            "img/content/8800.jpg",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Siemens ME45",
+        "answers": [
+            "img/content/SX1.jpg",
+            "img/content/me45.png",
+            "img/content/c65.jpg",
+            "img/content/m65.png",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Samsung C100",
+        "answers": [
+            "img/content/i710.webp",
+            "img/content/D600.jpg",
+            "img/content/c100.jpg",
+            "img/content/X600.webp",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Sony Ericsson T68i",
+        "answers": [
+            "img/content/K500i.webp",
+            "img/content/K790i.webp",
+            "img/content/M600i.jpg",
+            "img/content/T68i.webp",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "Motorola MPx200",
+        "answers": [
+            "img/content/MPx200.webp",
+            "img/content/razr_v3_3.jpg",
+            "img/content/SLVRL7.png",
+            "img/content/E398.jpg",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Sony Ericsson K500i",
+        "answers": [
+            "img/content/M600i.jpg",
+            "img/content/K790i.webp",
+            "img/content/K500i.webp",
+            "img/content/T68i.webp",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Motorola RAZR V3",
+        "answers": [
+            "img/content/E398.jpg",
+            "img/content/razr_v3_3.jpg",
+            "img/content/MPx200.webp",
+            "img/content/SLVRL7.png",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Siemens SX1",
+        "answers": [
+            "img/content/me45.png",
+            "img/content/m65.png",
+            "img/content/SL45.png",
+            "img/content/SX1.jpg",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "Motorola E398",
+        "answers": [
+            "img/content/MPx200.webp",
+            "img/content/E398.jpg",
+            "img/content/SLVRL7.png",
+            "img/content/razr_v3_3.jpg",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Siemens M65",
+        "answers": [
+            "img/content/m65.png",
+            "img/content/me45.png",
+            "img/content/SL45.png",
+            "img/content/SX1.jpg",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Sony Ericsson M600i",
+        "answers": [
+            "img/content/K500i.webp",
+            "img/content/T68i.webp",
+            "img/content/M600i.jpg",
+            "img/content/K750i.webp",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Nokia N70",
+        "answers": [
+            "img/content/N73.png",
+            "img/content/8800.jpg",
+            "img/content/3310.png",
+            "img/content/N70.png",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "Nokia 8800",
+        "answers": [
+            "img/content/8800.jpg",
+            "img/content/N70.png",
+            "img/content/3310.png",
+            "img/content/3250.webp",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Apple iPhone",
+        "answers": [
+            "img/content/iphone.png",
+            "img/content/4.webp",
+            "img/content/x.jpeg",
+            "img/content/xs.webp",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Sony Ericsson K790i",
+        "answers": [
+        "img/content/K790i.webp",
+            "img/content/K750i.webp",
+            "img/content/M600i.jpg",
+            "img/content/K500i.webp",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Motorola SLVR L7",
+        "answers": [
+            "img/content/E398.jpg",
+            "img/content/MPx200.webp",
+            "img/content/razr_v3_3.jpg",
+            "img/content/SLVRL7.png",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "Samsung D600",
+        "answers": [
+            "img/content/c100.jpg",
+            "img/content/X600.webp",
+            "img/content/D600.jpg",
+            "img/content/i710.webp",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Sony Ericsson K750i",
+        "answers": [
+            "img/content/K500i.webp",
+            "img/content/K750i.webp",
+            "img/content/M600i.jpg",
+            "img/content/K790i.webp",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Nokia 3250",
+        "answers": [
+            "img/content/8800.jpg",
+            "img/content/9210.jpg",
+            "img/content/N70.png",
+            "img/content/3250.webp",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "LG Chocolate (KG800)",
+        "answers": [
+            "img/content/KG800.webp",
+            "img/content/680.jpg",
+            "img/content/5PP.jpg",
+            "img/content/basic.jpg",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Nokia 9210 Communicator",
+        "answers": [
+            "img/content/8800.jpg",
+            "img/content/9210.jpg",
+            "img/content/9500.jpg",
+            "img/content/3250.webp",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Nokia N73",
+        "answers": [
+            "img/content/3250.webp",
+            "img/content/8800.jpg",
+            "img/content/N73.png",
+            "img/content/N70.png",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Nokia N90",
+        "answers": [
+            "img/content/N70.png",
+            "img/content/N90.png",
+            "img/content/N73.png",
+            "img/content/3250.webp",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Nokia 5300 XpressMusic",
+        "answers": [
+            "img/content/9210.jpg",
+            "img/content/5300.webp",
+            "img/content/N95.png",
+            "img/content/5800.webp",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Nokia N91",
+        "answers": [
+            "img/content/N91.png",
+            "img/content/N90.png",
+            "img/content/N73.png",
+            "img/content/5300.webp",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Nokia 6131",
+        "answers": [
+            "img/content/8800.jpg",
+            "img/content/6131.webp",
+            "img/content/3250.webp",
+            "img/content/9210.jpg",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Samsung i710",
+        "answers": [
+            "img/content/c100.jpg",
+            "img/content/X600.webp",
+            "img/content/i710.webp",
+            "img/content/D600.jpg",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Nokia 6300",
+        "answers": [
+            "img/content/N70.png",
+            "img/content/3310.png",
+            "img/content/3250.webp",
+            "img/content/6300.webp",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "Nokia 6120 classic",
+        "answers": [
+            "img/content/N70.png",
+            "img/content/6120.webp",
+            "img/content/6300.webp",
+            "img/content/3250.webp",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Nokia N95",
+        "answers": [
+            "img/content/8800.jpg",
+            "img/content/N95.png",
+            "img/content/6131.webp",
+            "img/content/3250.webp",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Nokia 7900 Prism",
+        "answers": [
+            "img/content/N73.png",
+            "img/content/7500.webp",
+            "img/content/7900.webp",
+            "img/content/9210.jpg",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Siemens SL45",
+        "answers": [
+            "img/content/SL45.png",
+            "img/content/m65.png",
+            "img/content/SX1.jpg",
+            "img/content/me45.png",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Nokia N82",
+        "answers": [
+            "img/content/N70.png",
+            "img/content/N95.png",
+            "img/content/N73.png",
+            "img/content/N82.png",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "HTC HD2",
+        "answers": [
+            "img/content/DesireS.jpg",
+            "img/content/Desire300.webp",
+            "img/content/EVO.jpg",
+            "img/content/HD2.webp",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "Nokia 7650",
+        "answers": [
+            "img/content/9210.jpg",
+            "img/content/3250.webp",
+            "img/content/7650.webp",
+            "img/content/N90.png",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Samsung Galaxy Note",
+        "answers": [
+            "img/content/GalaxyNote2.jpeg",
+            "img/content/GalaxyNote.png",
+            "img/content/GalaxyNote8.webp",
+            "img/content/GalaxyNote4.webp",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Nokia E71",
+        "answers": [
+            "img/content/E71.png",
+            "img/content/E72.png",
+            "img/content/E70.webp",
+            "img/content/7650.webp",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Samsung Galaxy S II",
+        "answers": [
+            "img/content/GalaxySIII.png",
+            "img/content/GalaxyS4.webp",
+            "img/content/GalaxySII.png",
+            "img/content/GalaxyS.webp",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "Nokia 5800",
+        "answers": [
+            "img/content/E70.webp",
+            "img/content/7650.webp",
+            "img/content/5800.webp",
+            "img/content/5300.webp",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Nokia 8110",
+        "answers": [
+            "img/content/6300.webp",
+            "img/content/6131.webp",
+            "img/content/8110.jpg",
+            "img/content/banana.jpeg",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Nokia 808",
+        "answers": [
+            "img/content/3250.webp",
+            "img/content/7650.webp",
+            "img/content/N95.png",
+            "img/content/808.webp",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "Nokia 5140",
+        "answers": [
+            "img/content/6120.webp",
+            "img/content/7650.webp",
+            "img/content/N82.png",
+            "img/content/5140.jpg",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "Nokia 6230",
+        "answers": [
+            "img/content/7650.webp",
+            "img/content/6120.webp",
+            "img/content/6230.webp",
+            "img/content/5140.jpg",
+        ],
+        "right": 3,
+        "factor": 1
+    }, {
+        "name": "Nokia 8310",
+        "answers": [
+            "img/content/8310.jpg",
+            "img/content/6230.webp",
+            "img/content/6120.webp",
+            "img/content/5140.jpg",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Nokia 9500 Communicator",
+        "answers": [
+            "img/content/9210.jpg",
+            "img/content/6230.webp",
+            "img/content/9500.jpg",
+            "img/content/5140.jpg",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Nokia N93",
+        "answers": [
+            "img/content/N90.png",
+            "img/content/N95.png",
+            "img/content/N93.jpg",
+            "img/content/N82.png",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "Siemens C65",
+        "answers": [
+            "img/content/c65.jpg",
+            "img/content/me45.png",
+            "img/content/m65.png",
+            "img/content/SX1.jpg",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "Samsung X600",
+        "answers": [
+            "img/content/c100.jpg",
+            "img/content/i710.webp",
+            "img/content/X600.webp",
+            "img/content/D600.jpg",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "iPhone XS",
+        "answers": [
+            "img/content/xs.webp",
+            "img/content/xsmax.jpg",
+            "img/content/xr.jpg",
+            "img/content/x.jpeg",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "iPhone XS Max",
+        "answers": [
+            "img/content/xsmax.jpg",
+            "img/content/xs.webp",
+            "img/content/x.jpeg",
+            "img/content/xr.jpg",
+        ],
+        "right": 0,
+        "factor": 1
+    }, {
+        "name": "iPhone XR",
+        "answers": [
+            "img/content/x.jpeg",
+            "img/content/xr.jpg",
+            "img/content/xsmax.jpg",
+            "img/content/8.jpeg",
+        ],
+        "right": 1,
+        "factor": 1
+    }, {
+        "name": "iPhone X",
+        "answers": [
+            "img/content/8.jpeg",
+            "img/content/xr.jpg",
+            "img/content/x.jpeg",
+            "img/content/8Plus.png",
+        ],
+        "right": 2,
+        "factor": 1
+    }, {
+        "name": "iPhone SE",
+        "answers": [
+            "img/content/4.webp",
+            "img/content/5.webp",
+            "img/content/5s.jpg",
+            "img/content/se.png",
+        ],
+        "right": 3,
+        "factor": 1
+    }]
+};
---
+++ script.js
@@ -0,0 +1,421 @@
+function random(from, to) {
+    return Math.floor(Math.random() * (to - from)) + from;
+}
+
+function groupBy(array, keyPath) {
+    return array.reduce((a, v) => {
+        const key = v[keyPath];
+        (a[key] = a[key] || []).push(v);
+        return a;
+    }, {})
+}
+
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+class Storage {
+    static save(level, name, email, score) {
+        const data = Storage.select();
+        const levelData = data[level] ? data[level] : {};
+        levelData[name] = {
+            name,
+            email,
+            score
+        }
+
+        data[level] = levelData;
+
+        localStorage.setItem("konturface", JSON.stringify(data));
+    }
+
+    static get(name) {
+        const data = Storage.select();
+        return data[name] || null;
+    }
+
+    static select() {
+        const data = localStorage.getItem("konturface")
+        return data ? JSON.parse(data) : {};
+    }
+}
+
+class Game {
+    constructor(config, levels) {
+        this.config = config;
+        this.level = levels[config.level];
+    }
+
+    async run() {
+        while (true) {
+            const register = await new RegistrationPage(this.config.levels).run();
+            const selectedLevel = levels[register.level];
+            const result = await new GamePage(register.user, this.config, this.shuffleAnswers([...selectedLevel])).run();
+            Storage.save(register.level, result.name, result.email, result.score);
+            await new ResultPage(result).run();
+        }
+    }
+
+
+    shuffleAnswers(array) {
+        const groups = groupBy(array, "factor");
+        return Object.keys(groups).sort((a, b) => parseFloat(a) - parseFloat(b)).reduce((a, v) => [...a, ...shuffle(groups[v])], []);
+    }
+}
+
+class RegistrationPage {
+    constructor(availableLevels) {
+        this.resolver = null;
+        this.page = document.querySelector(".registration");
+        this.submit = document.querySelector(".submit");
+        this.form = document.querySelector(".form");
+        this.availableLevels = availableLevels;
+        this.register = this.register.bind(this);
+    }
+
+    initialize() {
+        this.form.reset();
+        this.page.classList.remove("invisible");
+        this.submit.addEventListener("click", this.register);
+    }
+
+    run() {
+        this.initialize();
+        return new Promise(resolve => this.resolver = resolve);
+    }
+
+    dispose() {
+        this.page.classList.add("invisible");
+        this.submit.removeEventListener("click", this.register)
+    }
+
+    end(user) {
+        this.dispose();
+        this.resolver(user);
+    }
+
+    register() {
+        const data = new FormData(this.form);
+        const user = {};
+        for (let [key, value] of data.entries()) {
+            user[key] = value;
+        }
+
+        if (user.name && user.email) {
+            this.end({user, level: config.level});
+        }
+    }
+}
+
+class Timer {
+    constructor(time, lostCallback) {
+        this.spiner = document.querySelector(".spiner");
+        this.inner = document.querySelector(".spinner-inner");
+        this.mask = document.querySelector(".spinner-mask");
+        this.maskTwo = document.querySelector(".spinner-mask-two");
+        this.timer = document.querySelector(".timer");
+
+        this.time = time;
+        this.callback = lostCallback;
+        this.interval = 0;
+    }
+
+    start() {
+        clearInterval(this.interval);
+        this.state = this.time;
+        this.timer.textContent = this.time / 1000;
+        this.interval = setInterval(this.tick.bind(this), 1000);
+        this.startAnimation();
+    }
+
+    startAnimation() {
+        this.inner.style.animation = `inner ${this.time / 1000}s linear`;
+        this.mask.style.animation = `mask ${this.time / 1000}s linear`;
+        this.maskTwo.style.animation = `mask-two ${this.time / 1000}s linear`;
+    }
+
+    stop() {
+        this.stopAnimation();
+        clearInterval(this.interval);
+    }
+
+    stopAnimation() {
+        this.inner.style.animationPlayState = "paused";
+        this.mask.style.animationPlayState = "paused";
+        this.maskTwo.style.animationPlayState = "paused";
+        setTimeout(() => {
+            this.inner.style.animation = undefined;
+            this.mask.style.animation = undefined;
+            this.maskTwo.style.animation = undefined;
+        }, 750)
+    }
+
+    tick() {
+        this.state -= 1000;
+        this.timer.textContent = this.state / 1000;
+        if (this.state <= 0) {
+            this.stop();
+            this.callback();
+        }
+    }
+}
+
+class AnswerButton {
+    constructor(dom, callback) {
+        this.dom = dom;
+        this.root = dom.getElementsByClassName("button-inner")[0];
+        this.callback = callback;
+        this.reset();
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    disable() {
+        this.isDisabled = true;
+        this.dom.removeEventListener("click", this.handleClick);
+        this.dom.classList.add("disabled");
+    }
+
+    wrongAnswer() {
+        this.isDisabled = true;
+        this.dom.removeEventListener("click", this.handleClick);
+        this.dom.classList.add("hidden");
+    }
+
+    setAnswer(index, src, isRight) {
+        this.isDisabled = false;
+        this.dom.classList.remove("disabled");
+        this.dom.classList.remove("hidden");
+        this.index = index;
+        this.isRight = isRight;
+        this.clearAnswer();
+        const img = document.createElement("div");
+        img.style.backgroundImage = `url(${src})`;
+        img.classList.add("answer-img");
+
+        this.root.appendChild(img);
+        this.dom.addEventListener("click", this.handleClick);
+    }
+
+    clearAnswer() {
+        if (this.root.firstChild) {
+            this.root.removeChild(this.root.firstChild);
+        }
+    }
+
+    handleClick() {
+        if (!this.isDisabled) {
+            this.callback(this.index);
+        }
+    }
+
+    good() {
+        this.setState("good");
+    }
+
+    bad() {
+        this.setState("bad");
+    }
+
+    setState(state) {
+        this.reset();
+        this.dom.classList.add(state);
+    }
+
+    reset() {
+        this.dom.classList.remove("good");
+        this.dom.classList.remove("bad");
+    }
+}
+
+class GamePage {
+    constructor(user, config, tasks) {
+        this.resolver = null;
+        this.result = {
+            name: user.name,
+            email: user.email,
+            score: 0
+        };
+        this.rounds = [...tasks];
+        this.currentRoundIndex = 0;
+        this.lives = config.lives;
+
+        this.page = document.querySelector(".game");
+        this.taskContainer = document.querySelector(".task");
+        this.descriptionContainer = document.querySelector(".description");
+        this.livesContainer = document.querySelector(".lives");
+        this.answer = this.answer.bind(this);
+        this.answers = [1, 2, 3, 4].map(x => new AnswerButton(document.querySelector(`.answer-${x}`), this.answer));
+        this.help5050Button = document.querySelector(".help5050");
+        this.timer = new Timer(config.time, () => this.badAnswer(this.rounds[this.currentRoundIndex].right, null));
+
+        this.help5050 = this.help5050.bind(this);
+    }
+
+    initialize() {
+        this.help5050Button.classList.remove("invisible");
+        this.help5050Button.addEventListener("click", this.help5050);
+        this.page.classList.remove("invisible");
+        this.livesContainer.textContent = this.lives;
+    }
+
+    run() {
+        this.initialize();
+        this.renderRound(0);
+        return new Promise(resolve => this.resolver = resolve);
+    }
+
+    dispose() {
+        this.help5050Button.removeEventListener("click", this.help5050);
+        this.timer.stop();
+        this.clearTask();
+        this.clearDescription();
+        this.page.classList.add("invisible");
+    }
+
+    end() {
+        this.dispose();
+        this.resolver(this.result)
+    }
+
+    answer(number) {
+        this.timer.stop();
+        this.answers.map(x => x.disable());
+        const round = this.rounds[this.currentRoundIndex];
+        round.right === number ? this.rightAnswer(number, round.factor) : this.badAnswer(round.right, number);
+    }
+
+    nextRound() {
+        this.currentRoundIndex++;
+        if (this.currentRoundIndex === this.rounds.length) {
+            return this.end();
+        }
+        this.renderRound(this.currentRoundIndex);
+    }
+
+    rightAnswer(number, factor) {
+        this.answers[number].good();
+        this.result.score += this.lives * factor * this.timer.state / 100;
+        setTimeout(() => {
+            this.answers[number].reset();
+            this.nextRound();
+        }, 1000)
+    }
+
+    badAnswer(rightAnswer, number) {
+        this.answers[number].bad();
+        this.answers[rightAnswer].good();
+        setTimeout(() => {
+            this.answers[number].reset();
+            this.answers[rightAnswer].reset();
+            this.decrementLives();
+            if (this.lives > 0) {
+                this.nextRound()
+            } else {
+                this.end()
+            }
+        }, 2000);
+    }
+
+    decrementLives() {
+        this.lives = this.lives - 1;
+        this.livesContainer.textContent = this.lives;
+    }
+
+    renderRound(number) {
+        this.clearTask();
+        this.clearDescription();
+        this.taskContainer.appendChild(this.createTaskTag(this.rounds[number]));
+        this.descriptionContainer.appendChild(this.createDescriptionTag(this.rounds[number]));
+        this.rounds[number].answers.map((x, i) => this.answers[i].setAnswer(i, x, i === this.rounds[number].right));
+        this.timer.start();
+    }
+
+    clearTask() {
+        if (this.taskContainer.firstChild) {
+            this.taskContainer.removeChild(this.taskContainer.firstChild);
+        }
+    }
+
+    clearDescription() {
+        if (this.descriptionContainer.firstChild) {
+            this.descriptionContainer.removeChild(this.descriptionContainer.firstChild);
+        }
+    }
+
+    createTaskTag(task) {
+        const p = document.createElement("div");
+        p.textContent = task.name;
+        return p;
+    }
+
+    createDescriptionTag(task) {
+        const p = document.createElement("div");
+        p.textContent = task.description;
+        return p;
+    }
+
+    help5050() {
+        this.help5050Button.classList.add("invisible");
+        const firstWrongAnswer = this.answers.filter(x => !x.isRight)[random(0, 3)];
+        const secondWrongAnswer = this.answers.filter(x => !x.isRight && x.index !== firstWrongAnswer.index)[random(0, 2)];
+        firstWrongAnswer.wrongAnswer();
+        firstWrongAnswer.clearAnswer();
+        secondWrongAnswer.wrongAnswer();
+        secondWrongAnswer.clearAnswer();
+    }
+}
+
+class ResultPage {
+    constructor(result) {
+        this.resolver = null;
+        this.result = result;
+        this.page = document.querySelector(".gameover");
+        this.resultContainer = document.querySelector(".result");
+        this.button = document.querySelector(".end");
+        this.end = this.end.bind(this);
+    }
+
+    initialize() {
+        this.button.addEventListener("click", this.end);
+        this.page.classList.remove("invisible");
+        this.resultContainer.textContent = this.result.score;
+    }
+
+    run() {
+        this.initialize();
+        return new Promise(resolve => this.resolver = resolve);
+    }
+
+    dispose() {
+        this.button.removeEventListener("click", this.end);
+        this.page.classList.add("invisible");
+    }
+
+    end() {
+        this.dispose();
+        this.resolver();
+    }
+}
+
+const forms = document.querySelectorAll('form');
+forms.forEach(form => {
+    form.addEventListener('submit', e => e.preventDefault())
+});
+
+const game = new Game(config, levels);
+game.run();
